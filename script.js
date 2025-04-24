5571a44bc936033c3674674cb9286f15cf264ec8
abhishek <abhishekjaswal1122@gmail.com>
2025-04-24 18:02:55 +0530

    added

---
+++ script.js
@@ -0,0 +1,10 @@
+// const numbers = [1,2,3,4];
+// const abc = numbers.map(item => item =2);
+// console.log(abc)
+
+
+function longest(word){
+    let letter = word.split("");
+}
+let namee= "this is abhishek jaswal";
+console.log(longest(namee));
